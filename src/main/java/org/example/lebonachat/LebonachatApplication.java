f3e0a03699e25287e6cb2f45b7cbb6724fc247b6
Kah42255 <neb.kahina33@gmail.com>
2025-12-09 16:09:29 +0100

    Initial commit du nouveau projet

---
+++ src/main/java/org/example/lebonachat/LebonachatApplication.java
@@ -0,0 +1,13 @@
+package org.example.lebonachat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LebonachatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LebonachatApplication.class, args);
+    }
+
+}
